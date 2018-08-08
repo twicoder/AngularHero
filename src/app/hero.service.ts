cdd874cfad0c278ef44ac46864b87027430f4d2c
renqw <renqw@asiainfo.com>
2018-08-08 14:25:34 +0800

    实现路由控制

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,4 +17,10 @@ export class HeroService {
     return Observable.of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return Observable.of(HEROES.find(hero => hero.id === id));
+  }
+
 }
